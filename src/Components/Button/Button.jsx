3f06909a4a48bc8bf644c2f028ae922032c535f0
FrostyValue <saturmendezc@gmail.com>
2025-03-11 15:39:30 +0100

    Element button created with CSS styles and routes

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {useNavigate} from 'react-router-dom';
+import AppRoutes from "../../routes/AppRoutes";
+
+export default function Button({to}) {
+    const navigate = useNavigate();
+    let textButton = "";
+
+    const handleNavigation = () => {
+        if (to === "adopt") {
+            navigate('/adopt');
+        } else {
+            navigate('/');
+        }
+    };
+
+    textButton = to === "adopt" ? "¡Adoptame!" : "Volver a Inicio";
+
+    return (
+        <>
+        <button type="button" onClick={handleNavigation}>{textButton}</button>
+        <AppRoutes />
+        </>
+    );    
+};
---
+++ src/pages/AdoptPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Routes from "../routes/AppRoutes.jsx"
+import Button from "../Components/Button/Button.jsx";
+
+export default function AdoptPage() {
+    return (<>
+        <h1>Adopt Web</h1>
+        <Button to=""></Button>
+        </>
+    )
+};
