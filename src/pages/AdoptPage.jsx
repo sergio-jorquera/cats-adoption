16012ca5dbb01fedcfc73d4dcad1b6362b523146
FrostyValue <saturmendezc@gmail.com>
2025-03-17 12:26:27 +0100

    Header and footer added to adopt page

--- src/pages/AdoptPage.jsx
+++ src/pages/AdoptPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Button from "../Components/Button/Button.jsx";
+import Header from "../Components/Header/Header.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
 import './styles/AdoptPage.css'
 
 export default function AdoptPage() {
   return (
+    <>
+      <Header />
       <div id="mainAdopt">
         <div id="mainContent">
         <p>Página web actualmente en construcción:</p>
         <Button to=""></Button>
         </div>
       </div>
+      <Footer />
+      </>
   );
 };
