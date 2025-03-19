eb0af15124ae12f0e2c162020b6b66d8bdef3d07
sergio-jorquera <sjorkera@gmail.com>
2025-03-19 15:18:54 +0100

    theme dark light done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Routes from "./routes/AppRoutes"; // 👈 Renombrado para evitar conflicto
+import { ThemeProvider } from "./context/ThemeContext";
+import "./App.css";
 
 
 function App() {
   return (
-  <Routes />
+    <ThemeProvider>
+      <Routes />
+    </ThemeProvider>
+ 
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from "./../context/ThemeContext"
 import CatSlider from '../Components/CatSlider/CatSlider';
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 
 function HomePage() {
+  const { theme, toggleTheme } = useContext(ThemeContext); // Accede a toggleTheme
   return (
-    <div>
+    <div className={theme}>
       <Header/>
       <h1>Adopta un Gatito</h1>
+      <button onClick={toggleTheme}>Cambiar Tema</button>
       <CatSlider/>
       <Footer/>
   </div>
