f2dc61625c9639596ac83f23897f053adb44379c
KeisyRaiback <yatelodireok@gmail.com>
2025-03-26 14:02:51 +0100

    some things fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 import { ThemeProvider } from "./context/ThemeContext";
 import { FavoritesProvider } from "./pages/FavoritesContext";
+import Header from "./Components/Header/Header"; // Importa el Header
+import Footer from "./Components/Footer/Footer"; // Importa el Footer
 import "./App.css";
 
 function App() {
   return (
     <FavoritesProvider>
       <ThemeProvider>
+        <Header /> {/* Renderiza el Header */}
         <AppRoutes />
+        <Footer /> {/* Renderiza el Footer */}
       </ThemeProvider>
     </FavoritesProvider>
   );
