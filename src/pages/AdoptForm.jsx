651177dd1796a7b663652b4cdbc7d796c2f544b1
FrostyValue <saturmendezc@gmail.com>
2025-03-27 10:58:04 +0100

    fix: AdoptForm css converted.

--- src/pages/AdoptForm.jsx
+++ src/pages/AdoptForm.jsx
@@ -4,7 +4,7 @@ import Header from "../Components/Header/Header.jsx";
 import Footer from "../Components/Footer/Footer.jsx";
 import { LanguageContext } from "../context/LanguageContext.jsx";
 import { useContext } from "react";
-import "./styles/AdoptForm.css";
+import styles from "./styles/AdoptForm.module.css";
 import "./../App.css";
 
 const initialState = {
@@ -18,6 +18,10 @@ const initialState = {
 
 function formReducer(state, action) {
   return { ...state, [action.name]: action.value };
+}
+
+function isNotEmptyString(str) {
+  return str.trim().length > 0;
 }
 
 function langSelection(langEng) {
@@ -37,14 +41,15 @@ function langSelection(langEng) {
         message: "Message: ",
         terms: "Accept that we process your data to contact you.",
         submit: "Submit",
+        messagePlaceholder: "250 characters max.",
         errors: {
           fullname: "You must enter at least first and last name",
           email: "Please enter a valid email",
           phone: "Enter a valid phone number",
+          city: "Please enter a valid city",
           terms: "You must accept the terms and conditions",
-          
         },
-        documentTitle: "Adoption from",
+        documentTitle: "Adoption form",
         successMessage: "Data sent successfully. We will contact you soon.",
       }
     : {
@@ -60,18 +65,19 @@ function langSelection(langEng) {
         phone: "Número de teléfono:",
         city: "Localidad:",
         message: "Mensaje:",
+        messagePlaceholder: "Maximo 250 caracteres.",
         terms: "Acepta que tratemos sus datos para poder contactar con usted.",
         submit: "Enviar",
         errors: {
           fullname: "Debe ingresar al menos nombre y apellido",
           email: "Por favor, introduce un correo válido",
           phone: "Introduce un número de teléfono válido",
+          city: "Por favor, introduzca una localidad válida",
           terms: "Debes aceptar los términos y condiciones",
-         
-        
         },
         documentTitle: "Formulario de adopcíon",
-        successMessage: "Datos enviados correctamente. Le contactaremos en breve.",
+        successMessage:
+          "Datos enviados correctamente. Le contactaremos en breve.",
       };
 }
 
@@ -83,8 +89,8 @@ export default function AdoptPage() {
 
   useEffect(() => {
     document.title = text.documentTitle;
-  }, [langEng])
-  
+  }, [langEng]);
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
 
@@ -101,7 +107,6 @@ export default function AdoptPage() {
     e.preventDefault();
     let newErrors = {};
 
-
     if (
       !/^[a-zA-Z\s]+$/.test(formData.fullName) ||
       formData.fullName.trim().split(" ").length < 2
@@ -115,6 +120,10 @@ export default function AdoptPage() {
       newErrors.email = text.errors.email;
     }
 
+    if (!isNotEmptyString(formData.city)){
+      newErrors.city = text.errors.city;
+    }
+
     if (!/^\+?[0-9]{9,15}$/.test(formData.phone)) {
       newErrors.phone = text.errors.phone;
     }
@@ -126,81 +135,92 @@ export default function AdoptPage() {
 
     setErrors(newErrors);
     if (Object.keys(newErrors).length === 0) {
-      alert(text.successMessage); 
+      alert(text.successMessage);
     }
   };
 
   return (
     <>
       <Header />
-      <div className="mainAdopt">
-        <div className="mainLeft">
-          <div className="mainContentLeft">
-            <h1>{text.mainTitle}</h1>
-            <p className="description">{text.description}</p>
+      <div className={styles.mainAdopt}>
+        <div className={styles.mainLeft}>
+          <div className={styles.mainContentLeft}>
+            <h1 className={styles.primaryTitle}>{text.mainTitle}</h1>
+            <p className={styles.description}>{text.description}</p>
             <ul>
-              <li>
+              <li className={styles.list}>
                 <FiPhone /> {text.contactNumber}
               </li>
-              <li>
+              <li className={styles.list}>
                 <FiMail /> {text.contactEmail}
               </li>
-              <li>
+              <li className={styles.list}>
                 <FiMapPin /> {text.location}
               </li>
             </ul>
           </div>
         </div>
-        <div className="mainRight">
-          <div className="mainContentRight">
-            <h2>{text.formTitle}</h2>
-            <form onSubmit={handleSubmit} className="adopt-form">
-              <div className="formdiv-field">
+        <div className={styles.mainRight}>
+          <div className={styles.mainContentRight}>
+            <h2 className={styles.secondaryTitle}>{text.formTitle}</h2>
+            <form onSubmit={handleSubmit}>
+              <div className={styles.formdiv}>
                 <label>{text.fullName}</label>
                 <input
                   name="fullName"
                   value={formData.fullName}
                   onChange={handleChange}
-                  
                 />
                 {errors.fullName && (
-                  <p className="error-text">{errors.fullName}</p>
+                  <p className={styles.errorText}>{errors.fullName}</p>
                 )}
               </div>
-              <div className="formdiv-field">
+              <div className={styles.formdiv}>
                 <label>{text.email}</label>
                 <input
                   type="email"
                   name="email"
                   value={formData.email}
                   onChange={handleChange}
-                  
                 />
-                {errors.email && <p className="error-text">{errors.email}</p>}
+                {errors.email && (
+                  <p className={styles.errorText}>{errors.email}</p>
+                )}
               </div>
-              <div className="formdiv-field">
+              <div className={styles.formdiv}>
                 <label>{text.phone}</label>
                 <input
                   type="tel"
                   name="phone"
                   value={formData.phone}
                   onChange={handleChange}
-                 
                 />
-                {errors.phone && <p className="error-text">{errors.phone}</p>}
+                {errors.phone && (
+                  <p className={styles.errorText}>{errors.phone}</p>
+                )}
               </div>
-              <div className="formdiv-field">
+              <div className={styles.formdiv}>
                 <label>{text.city}</label>
                 <input
                   name="city"
                   value={formData.city}
                   onChange={handleChange}
-                  />
-                  {errors.city && <p className="error-text">{errors.city}</p>}
-                
+                />
+                {errors.city && (
+                  <p className={styles.errorText}>{errors.city}</p>
+                )}
               </div>
-    
-              <div className="formdiv-field">
+              <div className={styles.formdiv}>
+                <label>{text.message}</label>
+                <textarea
+                  className={styles.formTextarea}
+                  name="message"
+                  onChange={handleChange}
+                  placeholder={text.messagePlaceholder}
+                  maxLength={250}
+                />
+              </div>
+              <div className={styles.formdiv}>
                 <input
                   type="checkbox"
                   name="terms"
@@ -208,9 +228,11 @@ export default function AdoptPage() {
                   onChange={handleChange}
                 />
                 <label>{text.terms}</label>
-                {errors.terms && <p className="error-text">{errors.terms}</p>}
+                {errors.terms && <p className={styles.errorText}>{errors.terms}</p>}
               </div>
-              <button className="submitButton" type="submit">{text.submit}</button>
+              <button className={styles.submitButton} type="submit">
+                {text.submit}
+              </button>
             </form>
           </div>
         </div>
